173699d018e4c2cec2a3de1102a391266ad41ddf
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2023-09-28 09:57:25 +0100

    Add files via upload

--- 1_Whisper_AOAI_endpoint.py
+++ 1_Whisper_AOAI_endpoint.py
@@ -1,12 +1,27 @@
 import gradio as gr
+import os
+import openai
 
+# Setting Azure OpenAI endpoint parameters
+openai.api_base = os.getenv('OPENAI_API_BASE')
+openai.api_key = os.getenv('OPENAI_API_KEY')     
+openai.api_version = '2023-09-01-preview'
+openai.api_type = 'azure'
+
+# Transcription function
 def transcribe(audio):
-    return "Hello World"
+    with open(audio, "rb") as audio_file:
+        transcription = openai.Audio.transcribe(
+            file=audio_file,
+            deployment_id="laziz_whisper_we",
+            model="laziz_whisper_we"
+        )
+    # print(transcription["text"])
+    return transcription["text"]
 
+# Gradio interface
 demo = gr.Interface(
-    transcribe,
-    gr.Audio(source="microphone"),
-    "textbox",
+    transcribe, gr.Audio(source="microphone", type="filepath", label="Audio Recording"), "textbox",
     title="Demo App 1: Whisper model through Azure OpenAI endpoint",
     description="Record your speech via microphone and press the Submit button to transcribe it into text."
 )
